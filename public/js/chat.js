07342c4541bea9f86f20f3e82c98980bc4382c32
martin.novak <ragnarecek@gmail.com>
2018-04-01 21:52:18 -0500

    added typing animation

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,7 +7,7 @@ const editableMessage =
      <div class="send">[ Send ]</div>
    </div>`;
 
-const newResponse = response => `<div class="message ai"><div class="text">${response}</div></div>`;
+const newResponse = `<div class="message ai"><div class="text"><span class="typing"></span></div></div>`;
 
 const sendMessage = message => {
   if (!message) return;
@@ -15,9 +15,10 @@ const sendMessage = message => {
   $ ('.send').text ('[ Sending... ]');
   $.post ('/api/query', {message: message})
     .done (response => {
-      $ ('#messages').append (newResponse (response.response));
+      const newMessage = $ (newResponse);
+      $ ('#messages').append (newMessage);
+      typing (newMessage, response.response);
       $ ('.send').remove ();
-      addEditableMessage ();
       if (response.payload) {
         console.log ('query:', response.query);
         console.log ('  response:', response.response);
@@ -39,43 +40,26 @@ const sendMenuMessage = message => {
 const addEditableMessage = () => {
   const newMessage = $ (editableMessage).on ('keydown', event => event.keyCode === 13 ? sendMessage (event.target.innerText) : '');
   $ ('#messages').append (newMessage);
-  setTimeout(function() {
-    document.querySelector('div[contenteditable=true]').focus();
-  }, 0);
+  setTimeout(() => document.querySelector('div[contenteditable=true]').focus(), 0);
   $ ('.send').on ('click', event => sendMessage ($ (event.target).parent ().find ('.text').text ()));
 };
 addEditableMessage ();
 
+const stripHTML = html => {
+  const doc = new DOMParser().parseFromString(html, 'text/html');
+  return doc.body.textContent || "";
+};
 
-// https://speckyboy.com/css-javascript-text-animation-snippets/ number 5
-
-const type = () => {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
+const typing = (jQueryElement, fullText) => {
+  if (!jQueryElement.length || !fullText) return;
+  const target = jQueryElement.find ('.typing');
+  const strippedFullText = stripHTML (fullText);
+  const currentText = target.text ();
+  if (currentText !== strippedFullText) {
+    target.text (strippedFullText.substr (0, currentText.length+1));
+    setTimeout(() => typing (jQueryElement, fullText), 20);
   } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    jQueryElement.find ('.text').html (fullText);
+    addEditableMessage ();
   }
-
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-  var that = this;
-  var delta = 300 - Math.random() * 100;
-
-  if (this.isDeleting) { delta /= 2; }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
-
-  setTimeout(function() {
-    that.tick();
-  }, delta);
 };
